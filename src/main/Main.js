7b62f64278b6d20f176115f10c4cc8207f775743
ZackLeonardo <maiz9088@icloud.com>
2018-08-24 21:02:58 +0800

    IntroductionItem component.

--- src/main/Main.js
+++ src/main/Main.js
@@ -4,7 +4,7 @@ import { Dimensions } from "react-native";
 import EStyleSheet from "react-native-extended-stylesheet";
 
 import defaultTheme from "../base/styles/defaultTheme";
-import Test from "../../example/Avatar.example";
+import Test from "../../example/IntroductionItem.example";
 
 // define REM depending on screen width
 const { width } = Dimensions.get("window");
